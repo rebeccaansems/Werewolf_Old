cdaba4100563e47a3fc3b4399c76b9a2dabaf3c8
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-07-01 00:20:23 -0300

    Updated build editor

--- Assets/Editor/BuildSettings.cs
+++ Assets/Editor/BuildSettings.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEditor;
 using UnityEngine;
 using System.Collections;
-
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 public class BuildSettings : EditorWindow
 {
@@ -9,7 +11,8 @@ public class BuildSettings : EditorWindow
     public string[] options = new string[] { "Controller", "Server" };
     public int index = 0;
 
-    private static string path = "C:/Users/User/Desktop/Werewolf Builds";
+    private static string buildPath = "C:/Users/User/Desktop/Werewolf Builds";
+    private static string scenePath = "C:/Users/User/Documents/GitHub/Werewolf/Assets/Scenes";
 
     [MenuItem("Custom/Windows Build")]
     static void Init()
@@ -22,41 +25,53 @@ public class BuildSettings : EditorWindow
     {
         index = EditorGUILayout.Popup(index, options);
         if (GUILayout.Button("Build"))
-            InstantiatePrimitive();
+            ContollerServerSwitch();
     }
 
-    void InstantiatePrimitive()
+    void ContollerServerSwitch()
     {
         switch (index)
         {
             case 0:
-                BuildController();
+                Build("Controller");
                 break;
             case 1:
-                BuildServer();
-                break;
-            default:
-                Debug.LogError("Unrecognized Option");
+                Build("Server");
                 break;
         }
     }
 
-    public static void BuildController()
+    private static void Build(string type)
     {
-        string[] levels = new string[] { "Assets/Code/Client/Controller.unity" };
+        List<string> levels = new List<string>();
+
+        DirectoryInfo dir = new DirectoryInfo(scenePath);
+        FileInfo[] info = dir.GetFiles("*.unity");
+        foreach (FileInfo f in info)
+        {
+            if (f.FullName.Contains(type))
+            {
+                levels.Add(f.FullName.Replace("C:/Users/User/Documents/GitHub/Werewolf/", ""));
+            }
+        }
+        //int sceneCount = UnityEngine.SceneManagement.SceneManager.sceneCountInBuildSettings;
+        //for (int i = 0; i < sceneCount; i++)
+        //{
+        //    string scene = (UnityEngine.SceneManagement.SceneUtility.GetScenePathByBuildIndex(i));
+        //    if (scene.Contains(type))
+        //    {
+        //        levels.Add(scene);
+        //    }
+        //}
 
         // Build player.
-        BuildPipeline.BuildPlayer(levels, path + "/Controller.exe", BuildTarget.StandaloneWindows, BuildOptions.Development);
-
+        BuildPipeline.BuildPlayer(levels.ToArray(), buildPath + "/" + type + ".exe", BuildTarget.StandaloneWindows, BuildOptions.Development);
+        PostBuild();
     }
 
-    public static void BuildServer()
+    public static void PostBuild()
     {
-        string[] levels = new string[] { "Assets/Code/Server/Server.unity" };
-
-        // Build player.
-        BuildPipeline.BuildPlayer(levels, path + "/Server.exe", BuildTarget.StandaloneWindows, BuildOptions.Development);
-
+        Process.Start(buildPath);
     }
 
     [MenuItem("Custom/Reset Playerprefs")]
